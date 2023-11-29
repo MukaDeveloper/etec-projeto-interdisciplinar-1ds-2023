a77c61f06ca1095855cfaf87995c0f36352642a7
Muka <muka.bertoni@gmail.com>
2023-11-29 11:42:12 -0300

    feat: minor update and bug fix

--- js/scriptCalculadora.js
+++ js/scriptCalculadora.js
@@ -17,6 +17,15 @@ function calcular(event) {
         return alert("Insira números válido nos valores.");
     }
 
+    const bhaskaraRes = document.getElementById('bhaskaraRes');
+
+    // Caso o valor de A seja 0, então se trata de uma equação de primeiro grau
+    if (a === 0 || a === null) {
+        document.getElementById('x1').innerHTML = '';
+        document.getElementById('x2').innerHTML = '';
+        return deltaRes.textContent = "A equação inserida trata-se de uma equação de primeiro grau.";
+    }
+
     // Caso os números sejam válidos, o processamento de dados irá calcular o valor de delta
     // É definido uma variável recebendo o valor aritmético da fórmula do delta;
     var delta = b * b - 4 * a * c;
@@ -26,6 +35,9 @@ function calcular(event) {
     // permite interpolação de expressões dentro do texto, usando ${<expressão>} como podemos ver abaixo.
     deltaRes.textContent = `Δ = ${delta}`;
 
+    const x1 = document.getElementById('x1');
+    const x2 = document.getElementById('x2');
+
     // Aqui verificamos se o valor do resultado aritmético calculado acima é menor ou igual a 0
     if (delta <= 0) {
         // Caso seja menor ou igual a zero, seguindo a fórmula de Bhaskara, não tem como tirar a raiz de
@@ -33,6 +45,8 @@ function calcular(event) {
         // Sendo uma função, é necessário retornar um valor, porém, nesse caso não há necessidade do prefixo
         // 'return' pois é uma definição final, tornando, nesse caso a função em um processamento.
         bhaskaraRes.textContent = "Não há valor real para essa equação.";
+        document.getElementById('x1').innerHTML = '';
+        document.getElementById('x2').innerHTML = '';
     } else {
         // Caso o valor de delta seja maior que 0 (se não for maior ou igual a 0) é calculado os resultados
         // possíveis da fórmula, sendo x1 e x2 (mais ou menos, respectivamente, nesse caso)
@@ -40,16 +54,22 @@ function calcular(event) {
         var x2res = (-b - Math.sqrt(delta)) / (2 * a);
 
         // Por fim, é exibido o resultado utilizando 'string de modelo', onde x1 e x2 são IDs de tags do HTML5
-        x1.textContent = `x1 = ${x1res.toFixed(2)}`;
-        x2.textContent = `x2 = ${x2res.toFixed(2)}`;
+        x1.innerHTML = `x1 = ${x1res.toFixed(2)}`;
+        x2.innerHTML = `x2 = ${x2res.toFixed(2)}`;
     }
 }
 
-document.getElementById('limparBtn').addEventListener('click', function () {
-    x1.textContent = ` `;
-    x2.textContent = ` `;
-})
+function limpar(event) {
+    event?.preventDefault();
 
+    document.getElementById('idtextA').value = '';
+    document.getElementById('idtextB').value = '';
+    document.getElementById('idtextC').value = '';
+
+    document.getElementById('deltaRes').innerHTML = '';
+    document.getElementById('x1').innerHTML = '';
+    document.getElementById('x2').innerHTML = '';
+}
 
 // As linhas de código abaixo servem para aplicar a funcionalidade de apertar a tecla ENTER para executar a função 'calcular'
 document.getElementById('idtextA').addEventListener('keydown', function (event) {
@@ -64,6 +84,7 @@ document.getElementById('idtextC').addEventListener('keydown', function (event) 
     if (event.key === "Enter") calcular();
 })
 
+// Essa função serve para mudar o 'placeholder' dos 'inputs' se a largura da tela for menor que 400 pixels
 function handleResize() {
     var larguraDaJanela = window.innerWidth;
 
@@ -78,6 +99,8 @@ function handleResize() {
     }
 }
 
+// Adicionar um evento na janela para aplicar a função anterior
 window.addEventListener('resize', handleResize);
 
+// Chamada da função assim que a página for carregada
 handleResize();
